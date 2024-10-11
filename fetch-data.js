79365377c5ffa17905b3123a22b9890f6b7ba38e
Ntshovelo Nell <neilmunetwa@gmail.com>
2024-10-11 20:49:44 +0200

    Add files via upload

---
+++ fetch-data.js
@@ -0,0 +1,27 @@
+async function fetchUserData(){
+  
+const apiURL ='https://jsonplaceholder.typicode.com/users';
+
+const dataContainer = document.getElementById('api-data');
+
+try{
+    const response =await fetch(apiURL);
+    const users = await response.json();
+
+    dataContainer.innerHTML = '';
+
+    const userList =document.createElement('ul');
+    users.forEach(user =>{
+        const listItem =document.createElement('li');
+        listItem.textContent = user.name;
+        userList.appendChild(listItem);
+    });
+    dataContainer.appendChild(userList);
+}catch{
+    dataContainer.innerHTML ='Failed to load user data';
+    console.error('Error fetching user data');
+}
+
+    
+};
+document.addEventListener('DOMContentLoaded', fetchUserData);
